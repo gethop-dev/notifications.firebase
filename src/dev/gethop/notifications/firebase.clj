ef8ac8e7f2042903c804911611d15343764634d0
Mikel Elizondo <mikel.elizondo@magnet.coop>
2023-08-31 11:37:10 +0200

    Add debug level logging to firebase functions

--- src/dev/gethop/notifications/firebase.clj
+++ src/dev/gethop/notifications/firebase.clj
@@ -8,8 +8,8 @@
   (:import [com.google.auth.oauth2 ServiceAccountCredentials]
            [com.google.firebase FirebaseApp FirebaseOptions]
            [com.google.firebase.messaging
-            MulticastMessage MulticastMessage$Builder FirebaseMessaging
-            SendResponse FirebaseMessagingException]
+            FirebaseMessaging FirebaseMessagingException MulticastMessage
+            MulticastMessage$Builder SendResponse]
            [java.util UUID]))
 
 (def ^:const ^:private firebase-scope
@@ -73,6 +73,10 @@
    (zipmap recipient firebase-responses)))
 
 (defn- send-notification* [firebaseApp logger recipient firebase-message opts]
+  (log logger :debug ::firebase-send-notification* {:firebaseApp firebaseApp
+                                                    :recipient recipient
+                                                    :firebase-message firebase-message
+                                                    :opts opts})
   (let [multicast-message-builder (-> (MulticastMessage/builder)
                                       (.putAllData firebase-message)
                                       (.addAllTokens recipient)
@@ -83,9 +87,9 @@
     (if (= (.getSuccessCount response) (count recipient))
       {:success? true}
       (let [errors (firebase-responses->errors recipient (.getResponses response))]
-        (log logger :error :firebase-notification {:success-count (.getSuccessCount response)
-                                                   :error-count (.getFailureCount response)
-                                                   :errors errors})
+        (log logger :error ::firebase-notification {:success-count (.getSuccessCount response)
+                                                    :error-count (.getFailureCount response)
+                                                    :errors errors})
         {:success? false :errors errors}))))
 
 (defn- send-notification
@@ -94,14 +98,19 @@
          (s/valid? ::core/recipient recipient)
          (s/valid? ::core/message message)
          (s/valid? ::core/opts opts)]}
+  (log logger :debug ::pre-send-notification {:message message
+                                              :opts opts
+                                              :recipient recipient})
   (let [firebase-message (message->firebase-message message)
         results (->> (flatten (vector recipient))
                      (partition-all multicast-recipient-limit)
                      (pmap #(send-notification* firebaseApp logger % firebase-message opts)))]
     (if (every? :success? results)
       {:success? true}
-      {:success? false
-       :errors (reduce concat (keep :errors results))})))
+      (let [errors (reduce concat (keep :errors results))]
+        (log logger :error ::pre-send-notification-error {:errors errors})
+        {:success? false
+         :errors errors}))))
 
 (s/def ::firebaseApp #(instance? FirebaseApp %))
 (s/def ::send-notification-args (s/cat :firebaseApp ::firebaseApp
@@ -115,6 +124,7 @@
   :ret ::core/send-notification-ret)
 
 (defn- send-notification-async [firebaseApp logger recipient message opts]
+  (log logger :debug ::firebase-notification-async)
   (future
     (send-notification firebaseApp logger recipient message opts)))
 
