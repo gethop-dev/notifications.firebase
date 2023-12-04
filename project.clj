7b82fcb71764d3e69512bd1a1a488ff34f0c68d3
Mikel Elizondo <mikel.elizondo@magnet.coop>
2023-12-04 17:16:06 +0100

    Prepare for next development release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dev.gethop/notifications.firebase "0.1.13"
+(defproject dev.gethop/notifications.firebase "0.1.14-SNAPSHOT"
   :description "A duct library for managing notifications with Firebase Cloud Messaging"
   :url "http://github.com/gethop-dev/notifications.firebase"
   :min-lein-version "2.9.8"
